596b25be33a032554fba108c821f43df2ec1dba7
Jeremy <jaroberts@ksu.edu>
2021-10-29 09:22:00 -0500

    First application complete!

---
+++ slider_status.py
@@ -0,0 +1,22 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+
+from slider_status_ui import Ui_MainWindow
+
+class Window(QMainWindow, Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.connectSignalsSlots()
+
+    def connectSignalsSlots(self):
+        pass
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
